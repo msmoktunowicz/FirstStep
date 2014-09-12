99f43e357a7623b24fb653827e74a6c77d377e61
Tomek <zakrzewskitomek@wp.pl>
2014-09-12 11:50:12 +0200

    zrobilem dla Ciebie program

---
+++ sortowanie.rb
@@ -0,0 +1,23 @@
+def bubble_sort(tab)
+return tab if tab.size <= 1
+
+swap = true
+while swap do 
+	swap = false
+	0.upto(tab.size - 2) do |i|
+	
+	if tab[i] > tab[i+1]
+		
+		tab[i], tab[i+1] = tab[i+1], tab[i]
+		
+		swap = true
+	
+	end
+		end
+		break unless swap
+end
+
+ tab
+end
+
+p bubble_sort([2,0,3,5,7,9])
